ee3383c7ed15faca7ee462554e055ec84f6303e6
James Wang <jameswang2003@gmail.com>
2015-09-18 16:48:42 +0800

    Implement Read/Write and log

--- Taiji.js
+++ Taiji.js
@@ -1,8 +1,18 @@
+var dir = "/Users/JamesWang/Documents/workspace/Repo/Taiji/Taiji/";
+
 function p(msg) {
     console.log(msg);
 }
+function pTips(msg) {
+    //36: CYAN
+    console.log('\x1b[36m', msg,'\x1b[0m');
+}
+function pErr(msg) {
+    //41: red
+    console.log('\x1b[41m', msg,'\x1b[0m');
+}
 
-p("hello world! This is Taiji.js");
+pTips("This is Taiji used to interpret YANG");
 
 function write(file, str) {
     var fs = require('fs');
@@ -15,7 +25,19 @@ function write(file, str) {
     });
 }
 
+function read(file) {
+    var fs = require("fs");
+
+    // Synchronous read
+    return fs.readFileSync(file).toString();
+}
 
 
 //Test parts
-write("output", "this is output");
+var read_str = read(dir + "YANG");
+p("Read file and get string: " + read_str);
+
+write(dir + "schema", "this is an output schema file");
+write(dir + "html", "this is an output html File");
+
+//read("./Taiji/output2");
